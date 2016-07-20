375a49d4a2afef1de8f481f140ff31ecfdca8b16
Mark Lundin <mark@mark-lundin.com>
2016-07-20 15:36:56 +0100

    Added isSuported

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import video from './video-panorama'
 import image from './image-panorama'
 
-let isSupported = ( function () {
+let iosVersion = ( function() {
+  if (/iP(hone|od|ad)/.test(navigator.userAgent)) {
+    // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
+    var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+    return parseInt(v[1], 10)
+}else return Infinity
+} )()
+
+let supportsInlinePlayback = iosVersion >= 10
+
+let supportsWebGL = ( function () {
 
 	try {
 
@@ -14,6 +24,10 @@ let isSupported = ( function () {
 	}
 
 } )()
+
+let isSupported = supportsWebGL && supportsInlinePlayback
+
+if( !isSupported ) console.warn( 'This device device does not support panoramic content' )
 
 let pano = { video, image, isSupported }
 
